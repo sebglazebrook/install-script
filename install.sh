0e6bf2a341d59a1110afb08ad2fc5d0b71c59ce7
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2016-11-14 16:38:34 +1100

    install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+function install_homebrew() {
+  echo "installing homebrew"
+}
+
+function install_homebrew_bundle() {
+  echo "installing homebrew bundle"
+}
+
+function install_brew_based_dependencies() {
+  echo "installing brew based dependencies"
+}
+
+function install_vm() {
+  echo "installing vm"
+}
+
+install_homebrew
+install_homebrew_bundle
+install_brew_based_dependencies
+install_vm
